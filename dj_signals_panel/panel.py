c553548b8f2df0b509962ed4ccaca51d68c132db
Yasser Toruno <yasser@yassers-mbp.lan>
2026-02-18 00:07:01 -0500

    configure panel as plugin for dj-control-room

---
+++ dj_signals_panel/panel.py
@@ -0,0 +1,8 @@
+class SignalsPanel:
+    id = "dj_signals_panel"
+    name = "Signals Panel"
+    description = "Monitor and debug Django signals"
+    icon = "radio"
+
+    def get_url_name(self):
+        return "index"
