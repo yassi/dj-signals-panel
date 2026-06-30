a69c3f27012e381d91ab0bda1cfec745b5bb4a82
Yasser Toruno <yasser@yassers-mbp.lan>
2026-06-30 11:54:14 -0400

    hook up the rest of the base utils

--- dj_signals_panel/conf.py
+++ dj_signals_panel/conf.py
@@ -1,29 +1,9 @@
-from django.conf import settings
-from django.templatetags.static import static
-from django.utils.html import format_html, mark_safe
+from dj_control_room_base.core import PanelConfig
 
-DEFAULTS = {
-    "LOAD_DEFAULT_CSS": True,
-    "EXTRA_CSS": [],
-    "SIGNAL_MODULES": [],
-    "SHOW_SOURCE": False,
-}
-
-
-def get_config(key=None):
-    user_config = getattr(settings, "DJ_SIGNALS_PANEL_SETTINGS", {})
-    if key is None:
-        return user_config
-    return user_config.get(key, DEFAULTS[key])
-
-
-def get_css_context():
-    links = []
-    for path in get_config("EXTRA_CSS"):
-        url = path if path.startswith(("http://", "https://", "//")) else static(path)
-        links.append(format_html('<link rel="stylesheet" href="{}">', url))
-    return {
-        "dj_cr_load_default_css": get_config("LOAD_DEFAULT_CSS"),
-        "dj_cr_extra_css": mark_safe("\n".join(links)),
-        "dj_cr_show_source": get_config("SHOW_SOURCE"),
-    }
+panel_config = PanelConfig(
+    settings_key="DJ_SIGNALS_PANEL_SETTINGS",
+    defaults={
+        "SIGNAL_MODULES": [],
+        "SHOW_SOURCE": False,
+    },
+)
--- dj_signals_panel/models.py
+++ dj_signals_panel/models.py
@@ -1,14 +1,7 @@
-from django.db import models
+from dj_control_room_base.core import PanelPlaceholderModel
 
 
-class CeleryPanelPlaceholder(models.Model):
-    """
-    This is a fake model used to create an entry in the admin panel for dj_signals_panel.
-    When we register this app with the admin site, it is configured to simply load
-    the panel templates.
-    """
-
-    class Meta:
-        managed = False
+class SignalsPanelPlaceholder(PanelPlaceholderModel):
+    class Meta(PanelPlaceholderModel.Meta):
         verbose_name = "Dj Signals Panel"
         verbose_name_plural = "Dj Signals Panel"
